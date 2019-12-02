e6dffd5d697abb84af64e92cf3e3a89551645f6c
yangli0806 <58411142+yangli0806@users.noreply.github.com>
2019-12-02 22:19:56 +0800

    Update kmeans.c

--- kmeans.c
+++ kmeans.c
@@ -1,50 +1,4 @@
-/*****
-** kmeans.c
-** - a simple k-means clustering routine
-** - returns the cluster labels of the data points in an array
-** - here's an example
-**   extern int *k_means(double**, int, int, int, double, double**);
-**   ...
-**   int *c = k_means(data_points, num_points, dim, 20, 1e-4, 0);
-**   for (i = 0; i < num_points; i++) {
-**      printf("data point %d is in cluster %d\n", i, c[i]);
-**   }
-**   ...
-**   free(c);
-** Parameters
-** - array of data points (double **data)
-** - number of data points (int n)
-** - dimension (int m)
-** - desired number of clusters (int k)
-** - error tolerance (double t)
-**   - used as the stopping criterion, i.e. when the sum of
-**     squared euclidean distance (standard error for k-means)
-**     of an iteration is within the tolerable range from that
-**     of the previous iteration, the clusters are considered
-**     "stable", and the function returns
-**   - a suggested value would be 0.0001
-** - output address for the final centroids (double **centroids)
-**   - user must make sure the memory is properly allocated, or
-**     pass the null pointer if not interested in the centroids
-** References
-** - J. MacQueen, "Some methods for classification and analysis
-**   of multivariate observations", Fifth Berkeley Symposium on
-**   Math Statistics and Probability, 281-297, 1967.
-** - I.S. Dhillon and D.S. Modha, "A data-clustering algorithm
-**   on distributed memory multiprocessors",
-**   Large-Scale Parallel Data Mining, 245-260, 1999.
-** Notes
-** - this function is provided as is with no warranty.
-** - the author is not responsible for any damage caused
-**   either directly or indirectly by using this function.
-** - anybody is free to do whatever he/she wants with this
-**   function as long as this header section is preserved.
-** Created on 2005-04-12 by
-** - Roger Zhang (rogerz@cs.dal.ca)
-** Modifications
-** -
-** Last compiled under Linux with gcc-3
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -135,3 +89,13 @@ int *k_means(double **data, int n, int m, int k, double t, double **centroids)
 
    return labels;
 }
+
+
+
+
+
+
+
+
+
+
